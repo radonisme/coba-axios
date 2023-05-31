8d09ae1337accbeee563d7a848b273c1969d4e92
tareqh86 <tareqhabbyullah@gmail.com>
2023-05-31 09:39:07 +0700

    coba axios

---
+++ javascript.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+// API
+var url = 'https://reqres.in/api/users';
+
+// Pakai Fetch :
+// fetch(url + '/1')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data.data))
+//   .catch((error) => console.log('Error: ' + error));
+
+// Pakai Axios
+// axios
+//   .get(url + '/1')
+//   .then((data) => console.log(data.data))
+//   .catch((error) => console.log('error: ' + error));
+
+axios
+  .post(url, {
+    nama: 'Dwi',
+    umur: '20',
+  })
+  .then((data) => console.log(data));
+
+// axios
+//   .get(url + '/x')
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log('Error: ' + error));
